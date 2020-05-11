b7536343113b3333fcb8adee320f2d29bd2da639
MotiKoistine <61470366+MotiKoistine@users.noreply.github.com>
2020-05-11 12:34:20 +0300

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,7 @@
+public class Item{
+	String itemName;
+	int itemPrice = 100;
+	Item(String itemName){
+		this.itemName = itemName;
+	}
+}
--- MainProgram.java
+++ MainProgram.java
@@ -1,14 +1,22 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainProgram{
 	JFrame frame;
 	JPanel cont;
 	Font font;
-	JTextArea itemArea,infoArea;
+	JTextArea itemArea;
 	JTextField codeField;
 	JButton customerBtn,itemBtn,deliveryBtn,exitBtn;
+	JLabel infoArea;
+	List<Item> items;
 	
 	MainProgram(User user){
 		frame = new JFrame("OrderKoistine");
@@ -16,27 +24,81 @@ public class MainProgram{
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setSize(screenSize.width,screenSize.height);
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		font = new Font("Arial",Font.PLAIN,20);
+		font = new Font("Arial",Font.PLAIN,30);
 		cont = new JPanel();
 		cont.setLayout(null);
+		items = new ArrayList<Item>();
 		
 		itemArea = new JTextArea();
-		codeField = new JTextField(600);
+		codeField = new JTextField();
 		customerBtn = new JButton("Select customer");
 		itemBtn = new JButton("Search item");
-		deliveryBtn = new JButton("Configure delivery");
+		deliveryBtn = new JButton("Select delivery");
 		exitBtn = new JButton("Exit");
+		infoArea = new JLabel(user.username + " (" + user.userId + ")");
 		
 		setLayout();
+		displayInfo();
 		
+		codeField.addKeyListener(new KeyListener(){
+			@Override
+			public void keyTyped(KeyEvent e){}
+			@Override
+			public void keyPressed(KeyEvent e){
+				if(e.getKeyCode() == KeyEvent.VK_ENTER){
+					addItem();
+				}
+			}
+			@Override
+			public void keyReleased(KeyEvent e){}
+		});
+		
+		frame.add(cont);
 		frame.setVisible(true);
+		
+		codeField.requestFocus();
+	}
+	private void addItem(){
+		itemArea.append(codeField.getText() + "\n");
+		codeField.setText("");
+		items.add(new Item(codeField.getText()));
 	}
 	private void setLayout(){
+		//Set fonts
 		codeField.setFont(font);
 		customerBtn.setFont(font);
 		itemArea.setFont(font);
 		itemBtn.setFont(font);
 		deliveryBtn.setFont(font);
 		exitBtn.setFont(font);
+		infoArea.setFont(font);
+		
+		
+		//Set sizes
+		itemArea.setBounds(5,5,1910,800);
+		codeField.setBounds(5,805,1910,40);
+		customerBtn.setBounds(5,850,250,140);
+		itemBtn.setBounds(260,850,250,140);
+		deliveryBtn.setBounds(1410,850,250,140);
+		exitBtn.setBounds(1665,850,250,140);
+		infoArea.setBounds(515,850,885,140);
+		infoArea.setHorizontalAlignment(SwingConstants.CENTER);
+		itemArea.setEditable(false);
+		
+		//Add elements
+		cont.add(itemArea);
+		cont.add(codeField);
+		cont.add(customerBtn);
+		cont.add(itemBtn);
+		cont.add(deliveryBtn);
+		cont.add(exitBtn);
+		cont.add(infoArea);
+		
+	}
+	private void displayInfo(){
+		String un = infoArea.getText();
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+		LocalDate date = LocalDate.now();
+		infoArea.setText("<html>" + un + "<br/" + date.format(dateFormat) + "</html>");
 	}
 }
