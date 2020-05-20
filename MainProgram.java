235420d16f880c4fda17a02b8a98cd9631c5effe
Tuomas Sihvo <tuomas.sihvo@koistine.com>
2020-05-20 12:46:16 +0300

    Working on Edit row class

--- MainProgram.java
+++ MainProgram.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 import javax.swing.*;
 import java.time.LocalDate;
@@ -9,6 +10,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
 public class MainProgram{
 	JFrame frame,errorFrame;
@@ -19,10 +21,13 @@ public class MainProgram{
 	JButton customerBtn,itemBtn,deliveryBtn,exitBtn,errorBtn;
 	JLabel infoArea,timeArea,totalPriceLabel,errorLabel;
 	Item item;
+	EditRow editRow;
 	List<Item> items;
 	Customer customer;
 	SearchCustomer searchCustomer;
 	SearchItem searchItem;
+	DecimalFormatSymbols formatSymbols;
+	int pos;
 	
 	MainProgram(User user){
 		frame = new JFrame("OrderKoistine");
@@ -35,6 +40,7 @@ public class MainProgram{
 		cont = new JPanel();
 		cont.setLayout(null);
 		items = new ArrayList<Item>();
+		formatSymbols = new DecimalFormatSymbols(Locale.US);
 		
 		itemArea = new JTextArea();
 		priceArea = new JTextArea();
@@ -48,7 +54,7 @@ public class MainProgram{
 		exitBtn = new JButton("Exit");
 		infoArea = new JLabel(user.username + " (" + user.userId + ")");
 		timeArea = new JLabel("");
-		totalPriceLabel = new JLabel("Total: 0,00€");
+		totalPriceLabel = new JLabel("Total: 0.00€");
 		
 		setLayout();
 		displayInfo();
@@ -151,7 +157,7 @@ public class MainProgram{
 		updateItems();
 	}
 	private void updateItems() {
-		DecimalFormat df = new DecimalFormat("#.00");
+		DecimalFormat df = new DecimalFormat("#.00",formatSymbols);
 		double finalPrice = 0;
 		Iterator<Item> i = items.iterator();
 		codeField.setText("");
@@ -160,6 +166,7 @@ public class MainProgram{
 		amountArea.setText("");
 		totalPriceArea.setText("");
 		int hPos = 45;
+		pos = 0;
 		while(i.hasNext()) {
 			JButton editBtn = new JButton("Edit");
 			Item row = i.next();
@@ -175,108 +182,35 @@ public class MainProgram{
 			editBtn.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					editRow(row);
+					editRow = new EditRow(row);
+					getRow(pos-1);
 				}
 			});
 			frame.repaint();
+			pos++;
 		}
 		if(finalPrice != 0) {
 			totalPriceLabel.setText("Total: " + df.format(finalPrice) + "€");
 		}
 	}
-	private void editRow(Item item) {
-		JFrame editFrame = new JFrame("Edit row");
-		JPanel editCont = new JPanel();
-		JLabel lName,lPriceOut,lPriceIn,lAmount,lProfit,lDiscount,lDiscountP;
-		JTextField tPriceOut,tAmount,tDiscount;
-		JButton editBtn;
-		double profit = item.priceOut - item.priceIn;
-		int[] numbers = {KeyEvent.VK_NUMPAD0,KeyEvent.VK_NUMPAD1,KeyEvent.VK_NUMPAD2,KeyEvent.VK_NUMPAD3,KeyEvent.VK_NUMPAD4,
-				KeyEvent.VK_NUMPAD5,KeyEvent.VK_NUMPAD6,KeyEvent.VK_NUMPAD7,KeyEvent.VK_NUMPAD8,KeyEvent.VK_NUMPAD9,KeyEvent.VK_BACK_SPACE};
-		Font editFont = new Font("Arial",Font.PLAIN,20);
-		editFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		editFrame.setResizable(false);
-		editFrame.setSize(400,170);
-		lName = new JLabel(item.itemName);
-		lPriceOut = new JLabel("€");
-		lPriceIn = new JLabel(item.priceIn + "€");
-		lAmount = new JLabel("pcs.");
-		lProfit = new JLabel("Profit:" + profit + "€");
-		tDiscount = new JTextField("0,00");
-		lDiscount = new JLabel("Discount:");
-		lDiscountP = new JLabel("%");
-		tPriceOut = new JTextField("" + item.priceOut);
-		tAmount = new JTextField("" + item.amount);
-		editBtn = new JButton("OK");
-		editCont.setLayout(null);
-		tPriceOut.addKeyListener(new KeyListener() {
-			@Override
-			public void keyTyped(KeyEvent e) {
-			}
-			@Override
-			public void keyPressed(KeyEvent e) {
-			}
-			@Override
-			public void keyReleased(KeyEvent e) {
-				for(int i : numbers) {
-					if(e.getKeyCode() == i) {
-						if(!tPriceOut.getText().isEmpty()) {
-							DecimalFormat df = new DecimalFormat("0.00");
-							double discount = item.priceOut;
-							try{
-								discount = (item.priceOut - Double.parseDouble(tPriceOut.getText())) / item.priceOut * 100;
-							}catch(Exception e1) {}
-							tDiscount.setText(""+df.format(discount));
-							double profitChange = Double.parseDouble(tPriceOut.getText()) - item.priceIn;
-							lProfit.setText("Profit:" + profitChange + "€");
+	private void getRow(int rowPos) {
+		Thread t = new Thread() {
+			public void run() {
+				while(editRow.getRow() == null) {
+					try {
+						Thread.sleep(1000);
+						Item row = editRow.getRow();
+						if(row != null) {
+							items.set(rowPos, row);
+							updateItems();
 						}
+					} catch (InterruptedException e) {
+						
 					}
 				}
-				if(e.getKeyCode() == KeyEvent.VK_COMMA || e.getKeyCode() == KeyEvent.VK_DECIMAL) {
-					String field = tPriceOut.getText();
-					field = field.replace(',', '.');
-					tPriceOut.setText(field);
-				}
 			}
-		});
-		
-		lName.setBounds(5,5,400,30);
-		tPriceOut.setBounds(5,40,80,30);
-		lPriceOut.setBounds(85,40,20,30);
-		lDiscount.setBounds(110,40,100,30);
-		tDiscount.setBounds(195,40,60,30);
-		lDiscountP.setBounds(255,40,20,30);
-		tAmount.setBounds(305,40,40,30);
-		lAmount.setBounds(350,40,50,30);
-		lPriceIn.setBounds(5,80,100,30);
-		lProfit.setBounds(110,80,200,30);
-		editBtn.setBounds(300,80,70,30);
-		
-		lName.setFont(editFont);
-		lPriceOut.setFont(editFont);
-		lPriceIn.setFont(editFont);
-		lAmount.setFont(editFont);
-		lProfit.setFont(editFont);
-		lDiscount.setFont(editFont);
-		lDiscountP.setFont(editFont);
-		tPriceOut.setFont(editFont);
-		tAmount.setFont(editFont);
-		tDiscount.setFont(editFont);
-		editBtn.setFont(editFont);
-		
-		editCont.add(lName);
-		editCont.add(lPriceOut);
-		editCont.add(lPriceIn);
-		editCont.add(lAmount);
-		editCont.add(lProfit);
-		editCont.add(lDiscount);
-		editCont.add(lDiscountP);
-		editCont.add(tPriceOut);
-		editCont.add(tAmount);
-		editCont.add(tDiscount);
-		editCont.add(editBtn);
-		editFrame.add(editCont);
-		editFrame.setVisible(true);
+		};
+		t.start();
 	}
 	private void displayError(String err) {
 		errorFrame = new JFrame("Error");
